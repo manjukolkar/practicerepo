777347c60fe12cfa81f5931f1b3978195798adb4
manjukolkar <manjunathkolkar2019@gmail.com>
2025-01-01 21:04:09 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+echo "Hello guys welcome to Jenkins session" 
 echo "Hello guys welcome to Git session"  
 echo "Hello guys welcome to Git session"  
 echo "Hello guys welcome to Git session" 
