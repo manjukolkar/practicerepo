cfe40b35f81fa3799f9f1e442fc260a3f479a813
vinayaka2519 <vinayaka251996@gmail.com>
2025-01-04 11:00:13 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "hello guys welcome to Jenkins Classes" 
-echo "Hello guys welcome to Git session"  
-echo "Hello guys welcome to Git session"  
-echo "Hello guys welcome to Git session" 
+echo "Hello guys welcome to Git session -1"  
+echo "Hello guys welcome to Git session-2"  
+echo "Hello guys welcome to Git session-3" 
 echo "First line"
 echo "Hello guys welcome to Git script session" 
 echo "Hello guys welcome to Git script session" 
