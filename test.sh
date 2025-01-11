762d6a2ef90051f27cd10420827a592f73d4f3d7
vinayaka2519 <vinayaka251996@gmail.com>
2025-01-11 19:37:13 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo "Hello guys welcome to Git session"  
-echo "Hello guys welcome to Git session"  
-echo "Hello guys welcome to Git session" 
+echo "Hello guys welcome to Git class-1"  
+echo "Hello guys welcome to Git class-2"  
+echo "Hello guys welcome to Git class-3" 
 echo "First line"
 echo "Hello guys welcome to Git script session" 
 
