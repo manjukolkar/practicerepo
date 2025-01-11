4a482510f3d553b8b7ec17e103238b0469b54960
vinayaka <vinayaka251996@gmial.com>
2025-01-11 14:53:23 +0000

    file edited

--- test.sh
+++ test.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
-<<<<<<< HEAD
-echo "hello guys welcome to Jenkins Classes" 
-echo "Hello guys welcome to Git session -1"  
-echo "Hello guys welcome to Git session-2"  
-echo "Hello guys welcome to Git session-3" 
-echo "First line 1"
-echo "Hello guys welcome to Git script session" 
-=======
-
 echo "Hello guys welcome to Git class-1"  
 echo "Hello guys welcome to Git class-2"  
 echo "Hello guys welcome to Git class-3" 
 echo "First line"
->>>>>>> dev
 echo "Hello guys welcome to Git script session" 
